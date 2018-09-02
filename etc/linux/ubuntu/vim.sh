10510c2be5a718527fca14efdce4baf0b7e2e20a
Riki Kaida <kasnake1013@gmail.com>
2018-09-02 23:06:08 +0900

    fix vim.sh

--- etc/linux/ubuntu/vim.sh
+++ etc/linux/ubuntu/vim.sh
@@ -6,25 +6,29 @@ dein_install() {
 
     curl "$DEIN_INSTALL_SCRIPT" > installer.sh
     sh installer.sh ~/.vim/dein
+
+    rm installer.sh
 }
 
 install_vim() {
-    local -r DOWNLOAD_DIR=/opt
+    local -r DOWNLOAD_DIR=/opt/vim
     sudo apt install -y build-essential ncurses-dev lua5.2 lua5.2-dev
     git clone https://github.com/vim/vim $DOWNLOAD_DIR
 
-    cd $DOWNLOAD_DIR/vim
+    cd $DOWNLOAD_DIR
 
-    sudo $DOWNLOAD_DIR/vim/configure \
-        --with-features=huge \
-        --enable-multibyte \
-        --enable-fontset
-        --enable-luainterp=dynamic \
-        --enable-cscope \
-        --enable-gui-auto \
-        --enable-gtk2-check \
-        --with-x \
-        --prefix=/usr/local
+    if [ -f $DOWNLOAD_DIR/configure ]; then
+        sudo $DOWNLOAD_DIR/configure \
+            --with-features=huge \
+            --enable-multibyte \
+            --enable-fontset \
+            --enable-luainterp=dynamic \
+            --enable-cscope \
+            --enable-gui-auto \
+            --enable-gtk2-check \
+            --with-x \
+            --prefix=/usr/local
+    fi 
 
     sudo make
     sudo make install
